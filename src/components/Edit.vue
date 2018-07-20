13ecb0cdaa57dda2ffb11ae2873d1f4f68fa6171
Peerawat Thongpoem <37680268+sagotty@users.noreply.github.com>
2018-07-20 11:36:27 +0700

    eiei

---
+++ src/components/Edit.vue
@@ -0,0 +1,48 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar" title="Edit"/>
+
+        <ScrollView>
+            <StackLayout class="home-panel">
+                <CardView class="card" elevation="10" radius="10" ios:shadowRadius="3">
+                    <StackLayout horizontalAlignment="stretch">
+                        <Label style="padding-left: 10rem;" text="รายละเอียด" class="h3" height="30" />
+                        <Label style="padding-left: 15rem;" text="ชื่อฟาร์ม :" class="h5" height="50" />
+                        <Label style="padding-left: 15rem;" text="จำนวนโคทั้งหมด :" class="h5" height="50" />
+                        <Label style="padding-left: 15rem;" text="จำนวนโคพ่อพันธ์ุ :" class="h5" height="50" />
+                        <Label style="padding-left: 15rem;" text="จำวนวนโคแม่พันธุ์ :" class="h5" height="50" />
+                        <Label style="padding-left: 15rem;" text="จำนวนโคขุน :" class="h5" height="50" />
+                        <Label style="padding-left: 15rem;" text="จำนวนลูกโค :" class="h5" height="50" />
+
+                        <StackLayout orientation="horizontal" >
+                        <Button text="ตกลง" class="h3" @tap="Yeser" color="#121258" backgroundColor="#43b883" width="50%"/>
+                            <Button text="ยกเลิก" class="h3" @tap="Noway" color="#121258" width="50%"/>
+                    </StackLayout>
+
+                    </StackLayout>
+                </CardView>
+            </StackLayout>
+        </ScrollView>
+
+    </Page>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods : {
+            Yeser : function () {
+                this.$router.push('/home')
+            },
+            Noway : function () {
+                this.$router.push('/home')
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,64 @@
+<template>
+    <Page class="page">
+
+        <FlexboxLayout class="page">
+            <StackLayout class="form">
+                <Image src="~/images/logo.png" class="logo"></Image>
+
+                <StackLayout class="input-field">
+                    <TextField hint="Email" class="input"/>
+                    <StackLayout class="hr-light"></StackLayout>
+                </StackLayout>
+
+                <StackLayout class="input-field">
+                    <TextField hint="Password" class="input"/>
+                    <StackLayout class="hr-light"></StackLayout>
+                </StackLayout>
+
+                <Button text="Log In" @tap="login" color="#121258" backgroundColor="#43b883"/>
+            </StackLayout>
+            <StackLayout orientation="horizontal">
+                <Label text="Don’t have an account?"/>
+                <Label color="blue" text="Sign up" @tap="login"/>
+            </StackLayout>
+        </FlexboxLayout>
+
+    </Page>
+</template>
+
+<style scoped>
+    .home-panel {
+        vertical-align: center;
+        font-size: 20rem;
+        margin: 20rem;
+    }
+
+    .logo {
+        width: 150rem;
+        margin-bottom: 50rem;
+    }
+
+    .page {
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .form {
+        margin-left: 30;
+        margin-right: 30;
+        flex-grow: 2;
+        vertical-align: middle;
+    }
+</style>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            login: function () {
+                this.$router.push('/home')
+            },
+        }
+    }
+</script>
